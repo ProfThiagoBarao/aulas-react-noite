ab7ab012af3fa933f256611f2a34f46665f11f30
ProfThiagoBarao <126165664+ProfThiagoBarao@users.noreply.github.com>
2026-08-11 22:13:16 -0300

    Add files via upload

--- src/aula04.jsx
+++ src/aula04.jsx
@@ -10,6 +10,10 @@ class Aula04 extends Component {
             source={require('../assets/icon.png')}
             style={ styles.icon }
         />
+        <Image 
+            source={{ uri: 'https://media.gettyimages.com/id/2185386463/pt/foto/european-badger.jpg?s=2048x2048&w=gi&k=20&c=thnQC2yxS5Eev1rARu87-Yy6dQ6ciFVLdo1bNmhpNKI=' }}
+            style={ styles.icon }
+        />
 
       </View>
     );
@@ -24,4 +28,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  icon: {
+    width: 150,
+    height: 150,
+  }
+
 });
